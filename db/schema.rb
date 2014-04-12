b8ccadd0603eeb1e2947a292176a949b3e4c5320
Melissa Holmes <meltar@gmail.com>
2014-04-12 12:45:41 -0400

    Change columns to be floats instad of strings

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412155118) do
+ActiveRecord::Schema.define(version: 20140412164450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "photos", force: true do |t|
-    t.string   "latitude"
-    t.string   "longitude"
     t.datetime "photographed_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.float    "latitude"
+    t.float    "longtitude"
   end
 
 end
