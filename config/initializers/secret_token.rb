100b54c9d51ef1246168fd27405a48255f51218a
Melissa Holmes <meltar@gmail.com>
2014-04-12 10:24:32 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Earthspy::Application.config.secret_key_base = '58362bb6757c4ae3f8d4f672f8539a6acb4f497633cf3272f1e231ffc890276fc0c57770203f85402c85da07b7b7ae5a59a4d078954001886e5a4bba1429fcc0'
